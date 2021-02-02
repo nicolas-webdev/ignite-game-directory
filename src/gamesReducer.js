af72c47df833caf6dcd6eea44c5ae622eb4e7721
Nicolas <nicobsas+webdev@gmail.com>
2021-02-02 20:37:03 +0900

    initial setup

---
+++ src/gamesReducer.js
@@ -0,0 +1,16 @@
+const initState = {
+  poular: [],
+  newGames: [],
+  upcoming: [],
+};
+
+const gameReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+export default gamesReducer;
