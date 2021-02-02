1413c7b1c67a343055c1c49ccb3e5cc1d532eff7
Nicolas <nicobsas+webdev@gmail.com>
2021-02-02 23:50:15 +0900

    displaying components and styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,51 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+import Game from "../components/Game";
 
 const Home = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadGames());
-  });
+  }, [dispatch]);
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
   return (
     <div>
-      <h1>Home</h1>
+      <GameList>
+        <h2>Upcoming Games</h2>
+        <Games>
+          {upcoming.map((game) => (
+            <Game
+              key={game.id}
+              image={game.background_image}
+              name={game.name}
+              released={game.released}
+              id={game.id}
+            />
+          ))}
+        </Games>
+      </GameList>
     </div>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
