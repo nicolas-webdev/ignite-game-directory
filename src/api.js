2de6c726a5a78a173fdeec820638b8847c354441
Nicolas <nicobsas+webdev@gmail.com>
2021-02-04 20:29:34 +0900

    fetch details

--- src/api.js
+++ src/api.js
@@ -35,3 +35,5 @@ const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&pag
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${new_games}`;
+
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
