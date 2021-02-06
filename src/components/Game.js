c994c367418ed9fda4f7b48c15fcd58e2c2b9ac7
Nicolas <nicobsas+webdev@gmail.com>
2021-02-06 23:30:26 +0900

    add image resizing

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,8 @@ import { loadDetail } from "../actions/detailAction";
 
 import { Link } from "react-router-dom";
 
+import { smallImage } from "../util";
+
 const Game = ({ name, released, image, id }) => {
   //Load details
   const dispatch = useDispatch();
@@ -21,7 +23,7 @@ const Game = ({ name, released, image, id }) => {
       <Link to={`/game/${id}`}>
         <h3>{name}</h3>
         <p>{released}</p>
-        <img src={image} alt={name} />
+        <img src={smallImage(image, 640)} alt={name} />
       </Link>
     </StyledGame>
   );
--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { smallImage } from "../util";
 
 const GameDetail = () => {
   const history = useHistory();
@@ -38,14 +39,18 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img src={game.background_image} alt="image" />
+              <img src={smallImage(game.background_image, 1280)} alt="image" />
             </Media>
             <Description>
               <p>{game.description_raw}</p>
             </Description>
             <div className="gallery">
               {screen.results?.map((screen) => (
-                <img src={screen.image} alt={game.name} key={screen.id} />
+                <img
+                  src={smallImage(screen.image, 1280)}
+                  alt={game.name}
+                  key={screen.id}
+                />
               ))}
             </div>
           </Detail>
